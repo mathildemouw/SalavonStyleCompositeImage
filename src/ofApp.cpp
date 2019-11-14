d10eba49b1038664b9b6faeec1c7a65d0be94120
Mathilde <nihaomouw@mac.com>
2019-11-14 15:57:24 -0500

    Moon and sun together in composite

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -85,26 +85,26 @@ void ofApp::setup(){
           }
           moonComposite.allocate(640+640,480, OF_IMAGE_COLOR);
     
-//          for (int x = 0; x < 640; x++){
-//              for (int y = 0; y < 480; y++){
-//                  int avgR = 0;
-//                  int avgG = 0;
-//                  int avgB = 0;
-//                  for (int i = 0; i < moonImages.size(); i++){
-//                      ofColor color = moonImages[i].getColor(x,y);
-//                      avgR += color.r;
-//                      avgG += color.g;
-//                      avgB += color.b;
-//                  }
-//                  ofColor avg = ofColor( (float)avgR/(float)moonImages.size(),
-//                                        (float)avgG/(float)moonImages.size(),
-//                                        (float)avgB/(float)moonImages.size());
-//                  moonComposite.setColor(x,y,avg);
-//              }
-//          }
+          for (int x = 0; x < 640; x++){
+              for (int y = 0; y < 480; y++){
+                  int avgR = 0;
+                  int avgG = 0;
+                  int avgB = 0;
+                  for (int i = 0; i < moonImages.size(); i++){
+                      ofColor color = moonImages[i].getColor(x,y);
+                      avgR += color.r;
+                      avgG += color.g;
+                      avgB += color.b;
+                  }
+                  ofColor avg = ofColor( (float)avgR/(float)moonImages.size(),
+                                        (float)avgG/(float)moonImages.size(),
+                                        (float)avgB/(float)moonImages.size());
+                  moonComposite.setColor(x,y,avg);
+              }
+          }
     
-    //      // copy the pixels from RAM to the graphics card....
-//          moonComposite.update();
+        // copy the pixels from RAM to the graphics card....
+          moonComposite.update();
     
 }
 
@@ -120,7 +120,7 @@ void ofApp::draw(){
     composite.draw(0,480);
   
     moonImages[mouseX%moonImages.size()].draw(640,0);
-    moonComposite.draw(0,480);
+    moonComposite.draw(640,480);
 }
 
 //--------------------------------------------------------------
